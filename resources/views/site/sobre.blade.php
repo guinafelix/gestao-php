de54a479bc695e6f2fdc0151d3836ab2d9094cf1
guinafelix <oguilhermeleitefelix@gmail.com>
2024-02-05 18:22:37 -0300

    refactor: use assets

--- resources/views/site/sobre.blade.php
+++ resources/views/site/sobre.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>Super Gestão - Sobre</title>
+        <title>guinafelix - sobre</title>
         <meta charset="utf-8">
 
         <style>
