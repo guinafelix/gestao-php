b26e184ce38524b2c041f715d2504bf34f9c6679
guinafelix <oguilhermeleitefelix@gmail.com>
2024-02-19 15:20:09 -0300

    refactor: change site_contato form names

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,19 +1,26 @@
 <form action={{ route('site.contato') }} method="POST">
     @csrf
     <input type="nome" value="{{ old('nome') }}" placeholder="Nome" class="{{ $classe }}">
+    @if($errors->has('nome'))
+        {{ $errors->first('nome') }}
+    @endif
     <br>
-    <input type="telefone" value="{{ old('telefone') }}"" placeholder="Telefone" class="{{ $classe }}">
-    <br>
+    <input type="telefone" value="{{ old('telefone') }}" placeholder="Telefone" class="{{ $classe }}">
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
+    <br> 
     <input type="email" value="{{ old('email') }}" placeholder="E-mail" class="{{ $classe }}">
+    {{ $errors->has('email') ? $errors->first('email'): '' }}
     <br>
-    <select name="motivo_contato" class="{{ $classe }}">
+    <select name="motivo_contatos_id" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
         @foreach ($motivo_contatos as $key => $motivo_contato)
-            <option value="{{ $motivo_contato->id }}" {{ old('motivo_contato' == $motivo_contato->id  ? 'selected' : '' )}}>{{ $motivo_contato->motivo_contato }}</option>
+            <option value="{{ $motivo_contato->id }}" {{ old('motivo_contatos_id' == $motivo_contato->id  ? 'selected' : '' )}}>{{ $motivo_contato->motivo_contato }}</option>
         @endforeach
     </select>
+    {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id'): '' }}
     <br>
     <textarea name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '')  ? old('mensagem') : 'Preencha aqui a sua mensagem'}}</textarea>
+    {{ $errors->has('mensagem') ? $errors->first('mensagem'): '' }}
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
