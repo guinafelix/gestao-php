2661bc181634769e50b125c54716610ec2fee06a
guinafelix <oguilhermeleitefelix@gmail.com>
2024-02-19 13:58:50 -0300

    refactor: form view

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\MotivoContato;
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato() {
-        return view('site.contato');
+    public function contato(Request $request) {
+        $motivo_contatos = MotivoContato::all();
+        return view('site.contato', ['motivo_contatos'=> $motivo_contatos]);
+    }
+
+    public function salvar(Request $request) {
+        $request->validate([
+            'nome'=> 'required|string|max:40',
+            'telefone' => 'required',
+            'email'=> 'email',
+            'motivo_contato'=> 'required',
+            'mensagem'=> 'required|max:40',
+        ]);
+        SiteContato::create($request->all());
     }
 }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,19 +1,19 @@
 <form action={{ route('site.contato') }} method="POST">
     @csrf
-    <input type="nome" placeholder="Nome" class="{{ $classe }}">
+    <input type="nome" value="{{ old('nome') }}" placeholder="Nome" class="{{ $classe }}">
     <br>
-    <input type="telefone" placeholder="Telefone" class="{{ $classe }}">
+    <input type="telefone" value="{{ old('telefone') }}"" placeholder="Telefone" class="{{ $classe }}">
     <br>
-    <input type="email" placeholder="E-mail" class="{{ $classe }}">
+    <input type="email" value="{{ old('email') }}" placeholder="E-mail" class="{{ $classe }}">
     <br>
     <select name="motivo_contato" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
-        <option value="1">Dúvida</option>
-        <option value="2">Elogio</option>
-        <option value="3">Reclamação</option>
+        @foreach ($motivo_contatos as $key => $motivo_contato)
+            <option value="{{ $motivo_contato->id }}" {{ old('motivo_contato' == $motivo_contato->id  ? 'selected' : '' )}}>{{ $motivo_contato->motivo_contato }}</option>
+        @endforeach
     </select>
     <br>
-    <textarea name="mensage" class="{{ $classe }}">Preencha aqui a sua mensagem</textarea>
+    <textarea name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '')  ? old('mensagem') : 'Preencha aqui a sua mensagem'}}</textarea>
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
