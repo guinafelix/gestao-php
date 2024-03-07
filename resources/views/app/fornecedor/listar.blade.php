1abb7feb1ab8ad7cba121d19b4caeacc2fe85c06
guinafelix <oguilhermeleitefelix@gmail.com>
2024-03-07 10:41:09 -0300

    refactor: fornecedor table

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -4,30 +4,46 @@
 
 @section('conteudo')
 
-<div class="conteudo-pagina">
+  <div class="conteudo-pagina">
 
-    <div class="titulo-pagina-2">
-        <p>Fornecedor - Listar</p>
-    </div>
+      <div class="titulo-pagina-2">
+          <p>Fornecedor - Listar</p>
+      </div>
 
-    <div class="menu">
-        <ul>
-            <li><a href="{{ route('app.fornecedor.adicionar') }}">Novo</a></li>
-            <li><a href="{{ route('app.fornecedor') }}">Consulta</a></li>
-        </ul>
-    </div>
+      <div class="menu">
+          <ul>
+              <li><a href="{{ route('app.fornecedor.adicionar') }}">Novo</a></li>
+              <li><a href="{{ route('app.fornecedor') }}">Consulta</a></li>
+          </ul>
+      </div>
 
-    <div class="informacao-pagina">
-        <div style="width: 30%; margin-left: auto; margin-right: auto;">
-            <form method="POST" action="">
-                <input type="text" name="nome" placeholder="Nome" class="borda-preta" >
-                <input type="text" name="site" placeholder="Site" class="borda-preta" >
-                <input type="text" name="uf" placeholder="UF" class="borda-preta" >
-                <input type="text" name="email" placeholder="Email" class="borda-preta" >
-                <button type="submit" class="borda-preta">Pesquisar</button>
-            </form>
-        </div>
-    </div>
-</div>
+      <div class="informacao-pagina">
+          <table border="1" width="100%">
+              <thead>
+                  <tr>
+                      <th>Nome</th>
+                      <th>Site</th>
+                      <th>UF</th>
+                      <th>E-mail</th>
+                      <th></th>
+                      <th></th>
+                  </tr>
+              </thead>
 
-@endsection()    
+              <tbody>
+                  @foreach ($fornecedores as $fornecedor)
+                      <tr>
+                          <td>{{ $fornecedor->nome }}</td>
+                          <td>{{ $fornecedor->site }}</td>
+                          <td>{{ $fornecedor->uf }}</td>
+                          <td>{{ $fornecedor->email }}</td>
+                          <td>Excluir</td>
+                          <td><a href="{{ route('app.fornecedor.editar', $fornecedor->id) }}">Editar</a></td>
+                      </tr>
+                  @endforeach
+              </tbody>
+          </table>
+      </div>
+  </div>
+
+@endsection()
