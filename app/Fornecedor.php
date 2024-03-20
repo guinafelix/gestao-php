2f29edc43362aa9d915ff1884d1a93bc6f3e9320
guinafelix <oguilhermeleitefelix@gmail.com>
2024-03-20 15:39:27 -0300

    refactor: add eager loading

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     //
-    protected  $table = 'fornecedores';
+    protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
+    
+    public function produtos() {
+        return $this->hasMany('App\Item', 'fornecedor_id', 'id');
+    }
 }
