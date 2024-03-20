88f51c2d90c3f3d2847b732ff7cad64e0bc1d3de
guinafelix <oguilhermeleitefelix@gmail.com>
2024-03-20 16:23:31 -0300

    refactor: create and edit  fornecedor in produto blade

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Fornecedor;
 use App\Produto;
 use App\Item;
-use App\ProdutoDetalhe;
 use App\Unidade;
 use Illuminate\Http\Request;
 
@@ -19,7 +19,7 @@ class ProdutoController extends Controller
     {
         $produtos = Item::with(['itemDetalhe', 'fornecedor'])->paginate(10);
 
-        return response(view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all() ]));
+        return response(view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all()]));
     }
 
     /**
@@ -30,7 +30,8 @@ class ProdutoController extends Controller
     public function create()
     {
         $unidades = Unidade::all();
-        return response(view('app.produto.create', ['unidades' => $unidades]));
+        $fornecedores = Fornecedor::all();
+        return response(view('app.produto.create', ['unidades' => $unidades, 'fornecedores' => $fornecedores]));
     }
 
     /**
@@ -45,7 +46,8 @@ class ProdutoController extends Controller
             'nome' => 'required|min:3|max:40',
             'descricao' => 'required|min:3|max:2000',
             'peso' => 'required|integer',
-            'unidade_id' => 'exists:unidades,id'
+            'unidade_id' => 'exists:unidades,id',
+            'fornecedor_id' => 'exists:fornecedores,id'
         ];
 
         $feedback = [
@@ -55,12 +57,13 @@ class ProdutoController extends Controller
             'descricao.min' => 'O campo descrição deve ter no mínimo 3 caracteres',
             'descricao.max' => 'O campo descrição deve ter no máximo 2000 caracteres',
             'peso.integer' => 'O campo peso deve ser um número inteiro',
-            'unidade_id.exists' => 'A unidade de medida informada não existe'
+            'unidade_id.exists' => 'A unidade de medida informada não existe',
+            'fornecedor_id.exists' => 'O fornecedor informado não existe'
         ];
 
         $request->validate($regras, $feedback);
-        
-        Produto::create($request->all());
+
+        Item::create($request->all());
         return redirect()->route('produto.index');
     }
 
@@ -84,8 +87,8 @@ class ProdutoController extends Controller
     public function edit(Produto $produto)
     {
         $unidades = Unidade::all();
-        return response(view('app.produto.edit', ['produto' => $produto, 'unidades' => $unidades]));
-        //return view('app.produto.create', ['produto' => $produto, 'unidades' => $unidades]);
+        $fornecedores = Fornecedor::all();
+        return response(view('app.produto.edit', ['produto' => $produto, 'unidades' => $unidades, 'fornecedores' => $fornecedores]));
     }
 
     /**
@@ -95,10 +98,32 @@ class ProdutoController extends Controller
      * @param  \App\Produto  $produto
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Produto $produto)
+    public function update(Request $request, Item $produto)
     {
+        $regras = [
+            'nome' => 'required|min:3|max:40',
+            'descricao' => 'required|min:3|max:2000',
+            'peso' => 'required|integer',
+            'unidade_id' => 'exists:unidades,id',
+            'fornecedor_id' => 'exists:fornecedores,id'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute deve ser preenchido',
+            'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'descricao.min' => 'O campo descrição deve ter no mínimo 3 caracteres',
+            'descricao.max' => 'O campo descrição deve ter no máximo 2000 caracteres',
+            'peso.integer' => 'O campo peso deve ser um número inteiro',
+            'unidade_id.exists' => 'A unidade de medida informada não existe',
+            'fornecedor_id.exists' => 'O fornecedor informado não existe'
+        ];
+
+        $request->validate($regras, $feedback);
+
+        //dd($request->all());
         $produto->update($request->all());
-        return redirect()->route('produto.show', ['produto' => $produto->id ]);
+        return redirect()->route('produto.show', ['produto' => $produto->id]);
     }
 
     /**
