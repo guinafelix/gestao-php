777aba9dc6f7e9c2a775bccf76cb215a62581dca
guinafelix <oguilhermeleitefelix@gmail.com>
2024-04-02 08:33:31 -0300

    refactor: add column

--- resources/views/app/pedido/index.blade.php
+++ resources/views/app/pedido/index.blade.php
@@ -3,7 +3,7 @@
 @section('titulo', 'Pedido')
 
 @section('conteudo')
-    
+
     <div class="conteudo-pagina">
 
         <div class="titulo-pagina-2">
@@ -26,48 +26,54 @@
                             <th>Cliente</th>
                             <th></th>
                             <th></th>
+                            <th></th>
+                            <th></th>
                         </tr>
-                    </head>
+                        </head>
 
                     <tbody>
-                        @foreach($pedidos as $pedido)
+                        @foreach ($pedidos as $pedido)
                             <tr>
                                 <td>{{ $pedido->id }}</td>
                                 <td>{{ $pedido->cliente_id }}</td>
-                                <td><a href="{{ route('pedido.show', ['pedido' => $pedido->id ]) }}">Visualizar</a></td>
+                                <td><a href="{{ route('pedido-produto.create', ['pedido' => $pedido->id]) }}">Adicionar
+                                        Produtos</a></td>
+                                <td><a href="{{ route('pedido.show', ['pedido' => $pedido->id]) }}">Visualizar</a></td>
                                 <td>
-                                    <form id="form_{{$pedido->id}}" method="post" action="{{ route('pedido.destroy', ['pedido' => $pedido->id]) }}">
+                                    <form id="form_{{ $pedido->id }}" method="post"
+                                        action="{{ route('pedido.destroy', ['pedido' => $pedido->id]) }}">
                                         @method('DELETE')
                                         @csrf
                                         <!--<button type="submit">Excluir</button>-->
-                                        <a href="#" onclick="document.getElementById('form_{{$pedido->id}}').submit()">Excluir</a>
+                                        <a href="#"
+                                            onclick="document.getElementById('form_{{ $pedido->id }}').submit()">Excluir</a>
                                     </form>
                                 </td>
-                                <td><a href="{{ route('pedido.edit', ['pedido' => $pedido->id ]) }}">Editar</a></td>
+                                <td><a href="{{ route('pedido.edit', ['pedido' => $pedido->id]) }}">Editar</a></td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
-                
+
                 {{ $pedidos->appends($request)->links() }}
 
                 <!--
-                <br>
-                {{ $pedidos->count() }} - Total de registros por página
-                <br>
-                {{ $pedidos->total() }} - Total de registros da consulta
-                <br>
-                {{ $pedidos->firstItem() }} - Número do primeiro registro da página
-                <br>
-                {{ $pedidos->lastItem() }} - Número do último registro da página
+                    <br>
+                    {{ $pedidos->count() }} - Total de registros por página
+                    <br>
+                    {{ $pedidos->total() }} - Total de registros da consulta
+                    <br>
+                    {{ $pedidos->firstItem() }} - Número do primeiro registro da página
+                    <br>
+                    {{ $pedidos->lastItem() }} - Número do último registro da página
 
-                -->
+                    -->
                 <br>
-                Exibindo {{ $pedidos->count() }} pedidos de {{ $pedidos->total() }} (de {{ $pedidos->firstItem() }} a {{ $pedidos->lastItem() }})
+                Exibindo {{ $pedidos->count() }} pedidos de {{ $pedidos->total() }} (de {{ $pedidos->firstItem() }} a
+                {{ $pedidos->lastItem() }})
             </div>
         </div>
 
     </div>
 
 @endsection
-
