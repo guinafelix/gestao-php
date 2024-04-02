9431425e251c561f11305910a483a54b65b94667
guinafelix <oguilhermeleitefelix@gmail.com>
2024-04-02 13:00:03 -0300

    refactor: add table alias

--- app/Item.php
+++ app/Item.php
@@ -14,7 +14,18 @@ class Item extends Model
         return $this->hasOne('App\ItemDetalhe', 'produto_id', 'id');
     }
 
-    public function fornecedor() {
+    public function fornecedor()
+    {
         return $this->belongsTo('App\Fornecedor');
+    }
+
+    public function pedidos()
+    {
+        return $this->belongsToMany('App\Pedido', 'pedidos_produtos', 'produto_id', 'pedido_id');
+
+        /*
+            3 - Representa o nome da FK da tabela mapeada pelo model na tabela de relacionamento
+            4 - Representa o nome da FK da tabela mapeada pelo model utilizado no relacionamento que estamos implementando
+        */
     }
 }
