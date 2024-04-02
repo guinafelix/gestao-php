a52959cb560f6d15acc26ac6d738a0e0c12f98e8
guinafelix <oguilhermeleitefelix@gmail.com>
2024-04-02 15:01:22 -0300

    feat: delete item

--- resources/views/app/pedido_produto/create.blade.php
+++ resources/views/app/pedido_produto/create.blade.php
@@ -3,7 +3,7 @@
 @section('titulo', 'Pedido Produto')
 
 @section('conteudo')
-
+    
     <div class="conteudo-pagina">
 
         <div class="titulo-pagina-2">
@@ -20,8 +20,8 @@
         <div class="informacao-pagina">
             <h4>Detalhes do pedido</h4>
             <p>ID do pedido: {{ $pedido->id }}</p>
-            <p>Cliente: {{ $pedido->cliente_id }}</p>
-
+            <p>Cliente: {{ $pedido->cliente_id }}</p> 
+            
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
                 <h4>Itens do pedido</h4>
                 <table border="1" width="100%">
@@ -30,21 +30,29 @@
                             <th>ID</th>
                             <th>Nome do produto</th>
                             <th>Data de inclusão do item no pedido</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($pedido->produtos as $produto)
+                        @foreach($pedido->produtos as $produto)
                             <tr>
                                 <td>{{ $produto->id }}</td>
                                 <td>{{ $produto->nome }}</td>
                                 <td>{{ $produto->pivot->created_at->format('d/m/Y') }}</td>
+                                <td>
+                                    <form id="form_{{$produto->pivot->id}}" method="post" action="{{ route('pedido-produto.destroy', ['pedidoProduto' => $produto->pivot->id, 'pedido_id' => $pedido->id])}}">
+                                        @method('DELETE')
+                                        @csrf
+                                        <a href="#" onclick="document.getElementById('form_{{$produto->pivot->id}}').submit()">Excluir</a>
+                                    </form>
+                                </td>
                             </tr>
                         @endforeach
                     <tbody>
                 </table>
 
                 @component('app.pedido_produto._components.form_create', ['pedido' => $pedido, 'produtos' => $produtos])
-                @endcomponent
+                @endcomponent 
             </div>
         </div>
 
