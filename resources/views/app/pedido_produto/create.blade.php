e4d690786b819bde85eb47f2800fc1288146712e
guinafelix <oguilhermeleitefelix@gmail.com>
2024-04-02 08:58:03 -0300

    refactor: store function

--- resources/views/app/pedido_produto/create.blade.php
+++ resources/views/app/pedido_produto/create.blade.php
@@ -3,7 +3,7 @@
 @section('titulo', 'Pedido Produto')
 
 @section('conteudo')
-    
+
     <div class="conteudo-pagina">
 
         <div class="titulo-pagina-2">
@@ -20,15 +20,31 @@
         <div class="informacao-pagina">
             <h4>Detalhes do pedido</h4>
             <p>ID do pedido: {{ $pedido->id }}</p>
-            <p>Cliente: {{ $pedido->cliente_id }}</p> 
-            
+            <p>Cliente: {{ $pedido->cliente_id }}</p>
+
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
+                <h4>Itens do pedido</h4>
+                <table border="1" width="100%">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome do produto</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($pedido->produtos as $produto)
+                            <tr>
+                                <td>{{ $produto->id }}</td>
+                                <td>{{ $produto->nome }}</td>
+                            </tr>
+                        @endforeach
+                    <tbody>
+                </table>
                 @component('app.pedido_produto._components.form_create', ['pedido' => $pedido, 'produtos' => $produtos])
-                @endcomponent 
+                @endcomponent
             </div>
         </div>
 
     </div>
 
 @endsection
-
