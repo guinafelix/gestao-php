f9226f2ee16c246fea60d6a6fb1902cfe4bd8134
guinafelix <oguilhermeleitefelix@gmail.com>
2024-07-28 20:51:00 -0300

    refactor: add icon

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -4,6 +4,7 @@
 <head>
     <title>@yield('titulo')</title>
     <meta charset="utf-8">
+    <link rel="shortcut icon" href="{{ asset('img/logo-ia.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
 </head>
 
