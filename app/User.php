2858103c61c707466511b4906c3a38fdbedd788b
guinafelix <oguilhermeleitefelix@gmail.com>
2024-08-01 08:20:01 -0300

    feat: email verification config

--- app/User.php
+++ app/User.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use App\Notifications\VerifyEmailNotification;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
@@ -35,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new VerifyEmailNotification());
+    }
 }
