693ef9702ce95704ddb63f058c25447518d74a39
guinafelix <oguilhermeleitefelix@gmail.com>
2025-05-06 10:57:02 -0300

    refactor: update docker for production

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-until nc -z -v -w30 mysql 3306
-do
-  echo "Aguardando o MySQL iniciar..."
-  sleep 5
-done
+# until nc -z -v -w30 mysql 3306
+# do
+#   echo "Aguardando o MySQL iniciar..."
+#   sleep 5
+# done
 
 php artisan config:clear
 php artisan cache:clear
